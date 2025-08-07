23031ad426e8416a8a44a804cdc4462714b02bfd
menisadi <menisadi@gmail.com>
2025-08-07 12:51:14 +0300

    Refactor of zshrc and moved all functions to alias file

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -59,3 +59,162 @@ alias please="gum input --password | sudo -nS"
 alias du10="gum spin --spinner dot --title 'Scanning...' -- fd -t f . --exec du -h {} | sort -rh | head -n 10"
 
 alias sso-prod='aws sso login --profile prod && export AWS_PROFILE='\''prod'\'''
+
+
+# more versitle ls/eza version of the eza aliases
+function e() {
+  if [ -n "$1" ]; then
+    eza --sort=modified -r --icons=always | head -"$1"
+  else
+    eza --sort=modified -r --icons=always
+  fi
+}
+
+function ez() {
+  if [ -n "$1" ]; then
+    eza -l --sort=modified -r --icons=always | head -"$1"
+  else
+    eza -l --sort=modified -r --icons=always
+  fi
+}
+
+# Simple mkdir-cd combo
+mkcd() {
+    mkdir -p "$1" && cd "$1"
+}
+
+# create and open daily files
+todaymd() {
+  file="$(date +%Y-%m-%d).md"
+  [ ! -f "$file" ] && touch "$file"
+  ${EDITOR:-vim} "$file"
+}
+
+# My weather commands
+function weather() {
+  if [ "$1" = "-w" ]; then
+    curl "wttr.in/Herzliya?format=3"
+  elif [ "$1" = "-h" ]; then
+    curl "wttr.in/Yeruham?format=3"
+  elif [ -z "$1" ]; then
+    curl "wttr.in/?format=3"
+  else
+    echo "Usage: weather [-w|-h]"
+    echo "  -w: Get the weather for Herzliya"
+    echo "  -h: Get the weather for Yeruham"
+  fi
+}
+
+# Check for too large files 
+code2heavy() {
+  local path threshold exclude
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -path|--p)
+        shift
+        path="$1"
+        ;;
+      -threshold|--t)
+        shift
+        threshold="$1"
+        ;;
+      -exclude|--e)
+        shift
+        exclude="$1"
+        ;;
+      -help|--h)
+        echo "Usage: code2heavy [-path|--p <path>] [-threshold|--t <threshold>] [-exclude|--e <exclude>]"
+        return 0
+        ;;
+      *)
+        echo "Unknown option: $1"
+        return 1
+        ;;
+    esac
+    shift
+  done
+
+  if [[ -z "$path" ]]; then
+    echo "Please provide a valid path using --path or -p option."
+    return 1
+  fi
+
+  if [[ -z "$threshold" ]]; then
+    echo "Please provide a valid threshold using --threshold or -t option."
+    return 1
+  fi
+
+
+  if [[ -z "$exclude" ]]; then
+    /usr/bin/find "$path" -type f -size +"$threshold"M
+  else
+    /usr/bin/find "$path" -type d -name "$exclude" -prune -o -type f -size +"$threshold"M
+  fi
+}
+
+function record() {
+    local default_file="record.log"
+    
+    # Check if a command is provided
+    if [ -z "$1" ]; then
+        echo "No command provided. Usage: run_and_log 'command' [filename]"
+        return 1
+    fi
+
+    # Get the command to run
+    local command="$1"
+    
+    # Get the file name if provided, otherwise use the default
+    local filename="${2:-$default_file}"
+
+    # Run the command and use tee to write to the file and output to terminal
+    eval "$command" | tee "$filename"
+}
+
+# A function to use 'mods' to run shell commands
+runmods() {
+    generated_command=$(mods --role shell "$1")
+    echo "Generated command: $generated_command"
+    echo "Do you want to (r)un, (c)opy, or (q)uit?"
+    read -r action
+    
+    case $action in
+        r|R)
+            # Run the command
+            bash -c "$generated_command"
+            ;;
+        c|C)
+            # Copy the command to the clipboard (using pbcopy for MacOS)
+            echo "$generated_command" | pbcopy
+            echo "Command copied to clipboard."
+            ;;
+        q|Q)
+            echo "Canceled."
+            ;;
+        *)
+            echo "Invalid option. Please enter r, c, or q."
+            ;;
+    esac
+}
+
+modshell() {
+    command=$(mods --role shell "$@")
+    echo "Generated Command:"
+    echo "$command" | gum style --foreground 212 --padding "1 1" --italic
+
+    choice=$(gum choose "Run" "Copy" "Cancel")
+
+    case $choice in
+        "Run")
+            eval "$command"
+            ;;
+        "Copy")
+            echo "$command" | pbcopy  # 'pbcopy' is for macOS
+            echo "Command copied to clipboard."
+            ;;
+        "Cancel")
+            echo "Operation canceled."
+            ;;
+    esac
+}
