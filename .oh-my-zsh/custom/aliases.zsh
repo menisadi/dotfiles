6c90bcfa00eae2271aa91ee862e10a64585a80df
sadigu <menis@intentiq.com>
2026-01-20 09:25:44 +0200

    PGN analyzer alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -119,6 +119,7 @@ alias list_missing_docstrings="~/bin/list_missing_docstrings.py"
 alias logic_map="~/bin/logic_map.py"
 alias unused="~/bin/search_unused_files.py"
 alias nowp="~/bin/nowplaying.sh"
+alias analyze="~/bin/pgn_analyze.py"
 
 alias please="gum input --password | sudo -nS"
 alias du10="gum spin --spinner dot --title 'Scanning...' -- fd -t f . --exec du -h {} | sort -rh | head -n 10"
